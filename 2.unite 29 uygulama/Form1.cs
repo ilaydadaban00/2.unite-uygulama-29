eec62ac343c0e892daae31b5e02e6bebe4e661c6
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-03-06 05:58:13 +0300

    Add files via upload

---
+++ 2.unite 29 uygulama/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.unite_29_uygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int say = 1;
+            do
+            {
+                listBox1.Items.Add(say);
+                say++;
+            } while (say <= 10);
+
+        }
+
+       
+    }
+}
